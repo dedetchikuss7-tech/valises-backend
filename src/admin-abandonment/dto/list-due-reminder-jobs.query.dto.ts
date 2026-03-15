5bedd3a73664cf9edf77203ac8db7b26641687c2
Deniol Tchikoua <dedetchikuss7@gmail.com>
2026-03-15 18:32:03 +0100

    chore(admin-abandonment): improve swagger query param documentation

--- src/admin-abandonment/dto/list-due-reminder-jobs.query.dto.ts
+++ src/admin-abandonment/dto/list-due-reminder-jobs.query.dto.ts
@@ -1,12 +1,26 @@
 import { Type } from 'class-transformer';
 import { ReminderChannel } from '@prisma/client';
 import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class ListDueReminderJobsQueryDto {
+  @ApiPropertyOptional({
+    description: 'Filter due reminder jobs by channel',
+    enum: ReminderChannel,
+    example: ReminderChannel.INTERNAL,
+  })
   @IsOptional()
   @IsString()
   channel?: ReminderChannel | string;
 
+  @ApiPropertyOptional({
+    description: 'Maximum number of items to return',
+    minimum: 1,
+    maximum: 200,
+    default: 50,
+    example: 20,
+    type: Number,
+  })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
