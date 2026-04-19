3ae3905bb12effad4ba7be60795369ae1b790ed4
Deniol Tchikoua <dedetchikuss7@gmail.com>
2026-04-19 21:23:01 +0200

    feat(aml): add graduated restriction actions

--- src/aml/aml.module.ts
+++ src/aml/aml.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { TrustModule } from '../trust/trust.module';
 import { AmlController } from './aml.controller';
 import { AmlService } from './aml.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TrustModule],
   controllers: [AmlController],
   providers: [AmlService],
   exports: [AmlService],
